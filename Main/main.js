5b8c0d17e62ac1e323b5045b245d01acec956189
michaelgu95 <michaelgu95@gmail.com>
2014-11-05 15:30:51 -0500

    main project file

---
+++ Main/main.js
@@ -0,0 +1,174 @@
+var score = 0;
+var lives = new Array();
+var shapeDim = 75;
+
+var isNextable = true;
+var cols = 2; // starting columns
+var rows = 2; // starting rows
+var timeValue = 1200;
+var settingsShown = false;
+var activated = false;
+
+
+$(document).ready(function() {
+    next(cols, rows);
+    $('#restart').click(function(){
+        location.reload();
+    });
+
+    $('#resume').click(function(){
+        next(cols, rows);
+        $(".container").show();
+    });
+
+    for(i=0;i<3;i++){
+        lives[i] = new Image();
+        lives[i].src = 'life.png';
+        $("#lives").append($("<a>", {
+            html: $("<img>", { src: lives[i].src, height: 30, width: 30, class: 'animated rollIn' })
+        }));
+    }
+
+    $('.settingsIcon').click(function() {
+                $(".container").hide();
+                $("#resume").show();
+    });
+    
+    new jBox('Modal',{ //creates Settings box
+        width: 325,
+        height: 200,
+        attach: $('.settingsIcon'),
+        title: '<div class = "settingsText animated zoomIn"> Settings</div>',
+        content: $('#settings')
+    });
+
+    $(".timeSlider").bind("slider:changed", function (event, data) {
+    // The currently selected value of the slider
+        timeValue = data.value;
+    });
+});
+
+function endGame(cols, rows) {
+    $(".content").hide();
+    $(".FinishScreen").show();
+    $("#restart").fadeIn(3000);
+
+    $(".ScoreReminder").text("Your highest block: " + cols + "x" + rows);
+}
+
+function next(c, r) {
+    if (!isNextable) {
+        return;
+    }
+     var randomTime = Math.random()*800
+    $("#resume").hide();
+    $(".content").fadeOut(1500, function() { // time after every correct answer
+        $(".content").empty();
+        $(".container").animate({
+            height: ((shapeDim + 8) * r) + "px",
+            width: ((shapeDim + 8) * c) + "px"
+        }, randomTime, function() {
+            for (i = 0; i < (c * r); i++){
+                $(".content").append(createShape("circle", shapeDim));
+            }
+            $('html, body').animate({ 
+   				scrollTop: $(document).height()-$(window).height()}, "slow" // scroll to bottom after each level
+			);
+            $(".content").fadeIn(500);
+            pickWrongShapes();
+            pickRandomShapes();
+            isNextable = false;
+        })
+    });
+}
+
+function createShape(type, r) {
+    return $("<div>").addClass("shape " + type).width(r).height(r).click(function() {
+                
+		if(!activated){
+  				if ($(this).attr("selected") == "selected") {
+                    
+     				
+     				var score = parseInt($("#score").html());
+     				score += 20;
+     				$("#score").html(score); // update the score
+  				} else {
+     				
+     				var score = parseInt($("#score").html());
+     				score -= 10;
+     				$("#score").html(score); // update the score
+                    lives.splice(-1, 1);
+                    $('#lives a').eq(lives.length).remove();
+  				}
+			
+            if (lives.length <= 0){
+                return endGame(cols, rows);
+            }
+        }
+            
+               
+               
+
+                if ($(".wrong").length == 0) {
+                    if((cols+rows)%4 == 0 || (cols+rows)%4 == 1 || (cols+rows)%4 == 2){
+                        $(".content").html("<div style = 'position: absolute; left:800px; top: 265px'> <img class = 'animated lightSpeedIn' src = 'nyancat.gif' height = '80' width = '140'> </div>");
+                    } else if((cols+rows)%4 == 3 && !$('#checkSwitch').is(':checked')){
+                       $(".content").html("<div style = 'position: absolute; left: 785px; top: 280px'> <img class = 'animated wobble' src = '1up.png' height = '50' width = '50'> </div>");                    
+                       lives.splice(lives.length, 0, 1);
+                       $("#lives").append($("<a>", {
+                         html: $("<img>", { src: 'life.png', height: 30, width: 30, class: 'animated zoomIn' })
+                        }));
+                    }
+                    if(!$('#checkSwitch').is(':checked')){
+                        if (cols == rows) {
+                            cols++;
+                        }
+                        else if (cols > rows){
+                            rows++;
+                        }
+                    }
+                }
+                //next(cols, rows);
+            
+        
+    });
+}
+
+function pickRandomShapes() {
+    var count = 0;
+    var length = $(".content > .shape").length;
+    for (count = 0; count < Math.ceil(length / 3);) {
+        var random = Math.ceil(Math.random() * length);
+        if (random < length) {
+            if (!$(".content > .shape").eq(random).hasClass("selected")) {
+                $(".content > .shape").eq(random).addClass("selected").attr("selected", "selected");
+                count++;
+            }
+        }
+    }
+	
+    
+    window.setTimeout(hideRandomSelectedShapes, timeValue);
+    activated = false;
+    next(cols,rows)
+}
+
+function pickWrongShapes() {
+//Display red circles before selection 
+    var count = Math.ceil(Math.random()*(rows*cols))
+    //var length = $(".content > .shape").length;
+    for(i=0; i < count; i++)
+    {
+        if (!$(".content > .shape").eq(count).hasClass("selected")) 
+        {
+                $(".content > .shape").eq(count).addClass("wrong").attr("wrong", "wrong");
+        }
+    }
+}
+
+function hideRandomSelectedShapes() {
+        $(".content > .shape").removeClass("selected");
+        $(".content > .shape").removeClass("wrong");
+        
+        isNextable = true;
+}
